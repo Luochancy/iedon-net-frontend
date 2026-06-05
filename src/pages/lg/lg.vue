8eb1ce21f52750ed39a6d1caf29fee73654b599b
Lorien <lorien@luocynet.com>
2026-06-05 09:57:26 +0800

    Fix: LG search box flat prop to match nodes search style

--- src/pages/lg/lg.vue
+++ src/pages/lg/lg.vue
@@ -256,6 +256,7 @@ onMounted(() => {
                                         prepend-inner-icon="mdi-magnify"
                                         hide-details
                                         clearable
+                                        flat
                                         @keyup.enter="fetchRoutes"
                                     />
                                 </v-col>
